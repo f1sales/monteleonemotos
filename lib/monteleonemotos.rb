974649b0bf78b912ced707075d8ada3e2250be70
Nathanael Vieira <nathanael@f1sales.com.br>
2022-07-07 14:23:08 -0300

    Simplifying

--- lib/monteleonemotos.rb
+++ lib/monteleonemotos.rb
@@ -14,18 +14,7 @@ module Monteleonemotos
       lead_message = lead.message || ''
       lead_phone = lead.customer&.phone&.tr('^0-9', '') || ''
 
-      if source_name_down['honda']
-        # byebug
-        if source_name_down['ibiuna'] || source_name_down['sroque'] || source_name_down['são roque']
-          "#{source_name.split(' - ')[0..-2].join(' - ')} - Interior"
-        elsif source_name_down == 'website honda'
-          'myHonda - Website Honda'
-        else
-          source_name
-        end
-      elsif source_name_down['social']
-        'myHonda - Social'
-      elsif lead_message.include?('1010433')
+      if lead_message.include?('1010433')
         "#{source_name} - Mercês"
       elsif lead_message.include?('1622153')
         "#{source_name} - Sroque"
