2c2487d3fcaf449a5b4cdf1f331e672b5863dc09
Nathanael Vieira <nathanael@f1sales.com.br>
2022-07-07 13:02:19 -0300

    Unify sources

--- lib/monteleonemotos.rb
+++ lib/monteleonemotos.rb
@@ -3,6 +3,7 @@ require 'f1sales_custom/parser'
 require 'f1sales_custom/source'
 require 'f1sales_custom/hooks'
 require 'f1sales_helpers'
+require 'byebug'
 
 module Monteleonemotos
   class Error < StandardError; end
@@ -11,12 +12,23 @@ module Monteleonemotos
       source_name = lead.source.name
       source_name_down = source_name.downcase
       lead_message = lead.message || ''
-      lead_phone = lead.customer&.phone ? lead.customer.phone.tr('^0-9', '') : ''
+      lead_phone = lead.customer&.phone&.tr('^0-9', '') || ''
 
-      if lead_message.include?('1010433')
+      if source_name_down['honda']
+        # byebug
+        if source_name_down['ibiuna'] || source_name_down['sroque'] || source_name_down['são roque']
+          "#{source_name.split(' - ')[0..-2].join(' - ')} - Interior"
+        elsif source_name_down == 'website honda'
+          'myHonda - Website Honda'
+        else
+          source_name
+        end
+      elsif source_name_down['social']
+        'myHonda - Social'
+      elsif lead_message.include?('1010433')
         "#{source_name} - Mercês"
       elsif lead_message.include?('1622153')
-        "#{source_name} - São Roque"
+        "#{source_name} - Sroque"
       elsif lead_message.include?('1629248')
         "#{source_name} - Ibiuna"
       elsif source_name_down.include?('mobiauto')
@@ -25,8 +37,6 @@ module Monteleonemotos
         else
           "#{source_name} - Descarte"
         end
-      elsif source_name_down.include?('honda')
-        'Website Honda'
       else
         source_name
       end
