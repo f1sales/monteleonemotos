e53911169b2cdd9b784662c4629bb815ef262c62
Nathanael Vieira <nathanael@f1sales.com.br>
2022-07-12 17:26:46 -0300

    Standardize sources

--- spec/hooks_spec.rb
+++ spec/hooks_spec.rb
@@ -2,65 +2,14 @@ require 'webmock/rspec'
 require 'ostruct'
 
 RSpec.describe F1SalesCustom::Hooks::Lead do
-  context 'when the message contains code' do
-    let(:lead) do
-      lead = OpenStruct.new
-      lead.source = source
-      lead.message = 'Código da concessionária 1010433'
-
-      lead
-    end
-
-    let(:source_name) { 'Mercado Livre' }
-
-    let(:source) do
-      source = OpenStruct.new
-      source.name = source_name
-
-      source
-    end
-
-    let(:switch_source) { described_class.switch_source(lead) }
-
-    it 'When message is 1010433' do
-      expect(switch_source).to eq("#{source_name} - Mercês")
-    end
-
-    context 'When message is 1622153' do
-      before do
-        lead.message = 'Código da concessionária 1622153'
-      end
-
-      it 'When message is 1622153' do
-        expect(switch_source).to eq("#{source_name} - Sroque")
-      end
-    end
-
-    context 'When message is 1629248' do
-      before do
-        lead.message = 'Código da concessionária 1629248'
-      end
-
-      it 'When message is 1629248' do
-        expect(switch_source).to eq("#{source_name} - Ibiuna")
-      end
-    end
-
-    context 'When the message does not have the dealership code' do
-      before { lead.message = '' }
-
-      it 'When the message does not have the dealership code' do
-        expect(switch_source).to eq(source_name)
-      end
-    end
-  end
-
   context 'when needs to change source' do
     let(:lead) do
       lead = OpenStruct.new
       lead.source = source
       lead.customer = customer
       lead.message = nil
+      lead.description = nil
+      lead.product = product
 
       lead
     end
@@ -79,6 +28,13 @@ RSpec.describe F1SalesCustom::Hooks::Lead do
       customer.email = 'charizard@pokemon.com'
 
       customer
+    end
+
+    let(:product) do
+      product = OpenStruct.new
+      product.name = nil
+
+      product
     end
 
     let(:switch_source) { described_class.switch_source(lead) }
@@ -105,137 +61,88 @@ RSpec.describe F1SalesCustom::Hooks::Lead do
       end
     end
 
-    context 'when source is myHonda - Website Concessionária' do
-      let(:source_name) { 'myHonda - Website Concessionária' }
+    context 'when source is myHonda' do
+      let(:source_name) { 'myHonda' }
+      before { source.name = source_name }
 
-      context 'without city' do
+      context 'when message come with code 1629248' do
         before do
-          source.name = source_name
+          lead.message = 'Código da concessionária 1629248'
+          lead.description = 'Honda Social - Facebook'
         end
 
-        it 'return myHonda Website Concessionária' do
-          expect(switch_source).to eq(source_name)
+        it 'return myHonda - Website Honda - Ibiuna' do
+          expect(switch_source).to eq("#{source_name} - Website Honda - Ibiuna")
         end
       end
 
-      context 'when come from Sroque' do
+      context 'when message come with code 1622153' do
         before do
-          source.name = "#{source_name} - Sroque"
-        end
-
-        it 'return myHonda Website Concessionária - Sroque' do
-          expect(switch_source).to eq("#{source_name} - Sroque")
-        end
-      end
-
-      context 'when come from Ibiuna' do
-        before do
-          source.name = "#{source_name} - Ibiuna"
-        end
-
-        it 'return myHonda Website Concessionária - Ibiuna' do
-          expect(switch_source).to eq("#{source_name} - Ibiuna")
-        end
-      end
-
-      context 'Sroque with message 1622153' do
-        before do
-          source.name = "#{source_name} - São Roque"
           lead.message = 'Código da concessionária 1622153'
-        end
-
-        it 'return myHonda Website Concessionária - Sroque' do
-          expect(switch_source).to eq("#{source_name} - Sroque")
-        end
-      end
-    end
-
-    context 'when source name is myHonda - Peças' do
-      let(:source_name) { 'myHonda - Peças' }
-
-      context 'when come from Sroque' do
-        before do
-          source.name = "#{source_name} - Sroque"
-        end
-
-        it 'return myHonda - Peças - Sroque' do
-          expect(switch_source).to eq("#{source_name} - Sroque")
-        end
-      end
-    end
-
-    context 'when source name is myHonda - Serviços' do
-      let(:source_name) { 'myHonda - Serviços' }
-
-      context 'when come from Ibiuna' do
-        before do
-          source.name = "#{source_name} - Ibiuna"
-        end
-
-        it 'return myHonda - Serviços - Ibiuna' do
-          expect(switch_source).to eq("#{source_name} - Ibiuna")
-        end
-      end
-    end
-
-    context 'when source name is myHonda - Website Honda' do
-      let(:source_name) { 'myHonda - Website Honda' }
-
-      context 'when come from Mercês' do
-        before do
-          source.name = "#{source_name} - Mercês"
-          lead.message = 'Código da concessionária 1010433'
-        end
-
-        it 'return myHonda - Website Honda - Mercês' do
-          expect(switch_source).to eq("#{source_name} - Mercês")
-        end
-      end
-
-      context 'when come from São Roque' do
-        before do
-          source.name = 'Website Honda'
-          lead.message = 'Código da concessionária 1622153'
+          lead.description = 'Honda Social - Instagram'
         end
 
         it 'return myHonda - Website Honda - Sroque' do
-          expect(switch_source).to eq(source_name)
+          expect(switch_source).to eq("#{source_name} - Website Honda - Sroque")
         end
       end
-    end
 
-    context 'when source name is myHonda - Social' do
-      let(:source_name) { 'myHonda - Social' }
-
-      context 'without city' do
+      context 'when message come with message 1010433' do
         before do
-          source.name = source_name
+          lead.message = 'Código da concessionária 1010433'
+          lead.description = 'Website Honda - Site'
         end
 
-        it 'return myHonda - Social' do
-          expect(switch_source).to eq(source_name)
+        it 'return myHonda Website Concessionária - Mercês' do
+          expect(switch_source).to eq("#{source_name} - Website Honda - Mercês")
         end
       end
 
-      context 'when come from Mercês' do
+      context 'when message come with message 1629248' do
         before do
-          source.name = "#{source_name} - Mercês"
+          lead.message = 'Código da concessionária 1629248'
+          lead.description = 'Portal do Proprietário'
         end
 
-        it 'return myHonda - Social - Mercês' do
-          expect(switch_source).to eq("#{source_name} - Mercês")
+        it 'return myHonda Website Concessionária - Mercês' do
+          expect(switch_source).to eq("#{source_name} - Website Honda - Ibiuna")
         end
       end
 
-      context 'when come only Social in source name' do
+      context 'when description come with Website Honda - Site BHB' do
         before do
-          source.name = 'Social'
+          lead.message = 'Código da concessionária 1622153'
+          lead.description = 'Website Honda - Site BHB'
         end
 
-        it 'return myHonda - Social' do
-          expect(switch_source).to eq(source_name)
+        it 'return myHonda Website Concessionária - Mercês' do
+          expect(switch_source).to eq("#{source_name} - Website Honda - Sroque")
         end
       end
+
+      context 'when product name contains Peças e acessórios' do
+        before do
+          lead.message = 'Código da concessionária 1010433'
+          lead.description = 'WebSite Concessionária - Bot Whatsapp'
+          product.name = 'Peças e acessórios'
+        end
+
+        it 'return myHonda Website Concessionária - Mercês' do
+          expect(switch_source).to eq("#{source_name} - Peças - Mercês")
+        end
+      end
+
+      # context 'when product name contains Peças e acessórios' do
+      #   before do
+      #     lead.message = 'Código da concessionária 1629248'
+      #     lead.description = 'WebSite Concessionária - Bot Whatsapp'
+      #     product.name = 'XRE 300 - Financiamento'
+      #   end
+
+      #   it 'return myHonda Website Concessionária - Mercês' do
+      #     expect(switch_source).to eq("#{source_name} - Serviços - Ibiuna")
+      #   end
+      # end
     end
   end
 end
